abd9b9b0278861b31141d05af0dafc97a1b74716
sarbjeet <sarbjeets18@gmail.com>
2021-02-21 17:11:49 +0000

    label name change

--- app/src/main/java/com/example/a0321466/MainActivity.java
+++ app/src/main/java/com/example/a0321466/MainActivity.java
@@ -15,4 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    void f1(){
+
+    }
 }
